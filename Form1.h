77f9e1201d4a0674cf77c5c95cb15ec87ef33665
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-10 13:28:01 -0500

    first commit

---
+++ Form1.h
@@ -0,0 +1,116 @@
+#pragma once
+
+namespace spiwavform {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::NumericUpDown^  numericUpDown1;
+	protected: 
+	private: System::Windows::Forms::Label^  volume;
+	private: System::Windows::Forms::NumericUpDown^  numericUpDown2;
+	private: System::Windows::Forms::Label^  repeat;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->volume = (gcnew System::Windows::Forms::Label());
+			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->repeat = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->numericUpDown2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Location = System::Drawing::Point(29, 58);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(40, 20);
+			this->numericUpDown1->TabIndex = 0;
+			// 
+			// volume
+			// 
+			this->volume->AutoSize = true;
+			this->volume->Location = System::Drawing::Point(29, 39);
+			this->volume->Name = L"volume";
+			this->volume->Size = System::Drawing::Size(41, 13);
+			this->volume->TabIndex = 1;
+			this->volume->Text = L"volume";
+			// 
+			// numericUpDown2
+			// 
+			this->numericUpDown2->Location = System::Drawing::Point(95, 57);
+			this->numericUpDown2->Name = L"numericUpDown2";
+			this->numericUpDown2->Size = System::Drawing::Size(39, 20);
+			this->numericUpDown2->TabIndex = 2;
+			// 
+			// repeat
+			// 
+			this->repeat->AutoSize = true;
+			this->repeat->Location = System::Drawing::Point(95, 38);
+			this->repeat->Name = L"repeat";
+			this->repeat->Size = System::Drawing::Size(37, 13);
+			this->repeat->TabIndex = 3;
+			this->repeat->Text = L"repeat";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(292, 140);
+			this->Controls->Add(this->repeat);
+			this->Controls->Add(this->numericUpDown2);
+			this->Controls->Add(this->volume);
+			this->Controls->Add(this->numericUpDown1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->numericUpDown2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
+
